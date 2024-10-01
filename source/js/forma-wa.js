646e0c9581d352044dadba98a6d392bc9837b532
OKuzminova <yagodka029@yandex.ru>
2024-10-01 17:02:22 +0300

    Result work in Zoom 01.10.2024

--- source/js/forma-wa.js
+++ source/js/forma-wa.js
@@ -2,13 +2,14 @@
 const form = document.querySelector('.form');
 const number = '79788128290';
 
-function sendToWhatsapp(text, email, message, phone) {
+function sendToWhatsapp(text, num, email, message, phone) {
 
   text = "My name: " + encodeURIComponent(text) + "." + "%0a";
+  num = "Number: " + encodeURIComponent(num) + "." + "%0a";
   email = "Email: " + encodeURIComponent(email) + "." + "%0a";
   message = "Message: " + encodeURIComponent(message);
 
-  let url = `https://web.whatsapp.com/send?phone=${phone}&text=${text}${email}${message}&source=&data=`;
+  let url = `https://web.whatsapp.com/send?phone=${phone}&text=${text}${num}${email}${message}&source=&data=`;
 
   window.open(url);
 }
@@ -17,8 +18,9 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const text = e.currentTarget.querySelector('.name').value;
+  const num = e.currentTarget.querySelector('.number').value;
   const email = e.currentTarget.querySelector('.email').value;
   const message = e.currentTarget.querySelector('.message').value;
 
-  sendToWhatsapp(text, email, message, number);
+  sendToWhatsapp(text, num, email, message, number);
 });
